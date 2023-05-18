1af5db00e94c7e476afd59f89b55643cd4a464f4
dawidperla <davidperlauk@gmail.com>
2023-05-18 02:31:41 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res){
+    var today = new Date();
+    if (today.getDay() === 6 || today.getDate() === 0){
+        res.send("Weekend");
+    }else{
+        res.send("Working day")
+    }
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+})
